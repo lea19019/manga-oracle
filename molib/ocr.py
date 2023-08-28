274b2e67069331c220e48a0b48c7ea2cd95acafc
Adrian <vadriancastillo2@gmail.com>
2023-08-28 08:14:50 -0600

    Figuring out how get text from images

--- molib/ocr.py
+++ molib/ocr.py
@@ -1,6 +1,19 @@
 # Module for EasyOCR
 import easyocr
+import os
 reader = easyocr.Reader(['en',]) # this needs to run only once to load the model into memory
-result = reader.readtext('./jjk-manga/231/jjk_231_002.png')
-for r in result:
-    print(r)
+# Open folder ./jjk-manga/231/ and for every image in the folder, run the OCR
+path = "./jjk-manga/231/"
+# for filename in os.listdir(path):
+#     if filename.endswith(".png"):
+#         print(filename)
+#         # result = reader.readtext(path+filename)
+#         # print(result)
+#         continue
+#     else:
+#         continue
+
+
+result = reader.readtext('./jjk-manga/231/jjk_231_002.png', paragraph=True, detail=0)
+for res in result:
+    print(res)
